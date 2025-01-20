fd73677b28bef37cb0a77e5d3d78412a97d7ea1e
Susanna Rajamäki <strajama@gmail.com>
2025-01-20 11:38:28 +0200

    dist mukaan

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 const cors = require('cors')
 
 app.use(cors())
+
+app.use(express.static('dist'))
   
   let persons =   [
       {
